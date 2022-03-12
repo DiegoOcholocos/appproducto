66ac3dcc224725582f82fef4b37a7472b7c1c246
= <=>
2022-03-12 10:08:42 -0500

    primer git

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using appproducto.Models;
+
+namespace appproducto.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Consultar()
+    {
+        ViewData["Message"] =  "Gracias por realizar su compra";
+        return View("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
